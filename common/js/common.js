ecec6e4f6de5bd097298d9bdf3d632b5fda1373f
jeonsa <wjstmddk0708@gamil.com>
2019-04-15 03:20:41 +0900

    로딩

--- common/js/common.js
+++ common/js/common.js
@@ -5,7 +5,8 @@ $(window).resize(function() {
 var $eventShow = $("#eventShow");
 var $mainPage = $("#mainPage");
 var $phone = $(".phone");
-$(document).ready(function(){
+var $loading = $(".loading");
+$(window).load(function(){
     var flexHeight = $phone.height();
     var flexWidth = flexHeight*0.55;
     var $windowWidth = $(".WebView").width();
@@ -24,20 +25,16 @@ $(document).ready(function(){
     $(".linkBtn").on("click",function(){
         var link = "happyEvent/"+$(this).data("event")+"/index.html";
         $eventShow.attr("src",link);
-        $eventShow.show();
         $mainPage.hide();
-
-        /* var filter = "win16|win32|win64|mac|macintel";
-        if ( navigator.platform ) { 
-            if ( filter.indexOf( navigator.platform.toLowerCase() ) < 0 ) { 
-                location.href=link;
-            } else { 
-                $eventShow.attr("src",link);
-                $eventShow.show();
-                $mainPage.hide();
-            }
-        } */
-
+        $loading.show();
+        $eventShow.show();
+        
+        $eventShow.on('load', function(){
+        	$eventShow.removeClass("visibility_hidden");
+        	$loading.hide();
+        });
+        
+       
     });
     
     $(".btn_home").on("click",function(){
@@ -46,16 +43,3 @@ $(document).ready(function(){
     
     
 });
-
-/*window.onbeforeunload = function(e) {
-	alert(window.location.href);
-};*/
-
-/*$(window).bind("pageshow", function (event) {
-	if (event.originalEvent.persisted) {
-		console.log('BFCahe로부터 복원됨');
-	}
-	else {
-		console.log('새로 열린 페이지');
-	}
-});*/
