52d01061eeb3c16214b1b75a019b8623ed8c0ca3
jeonsa <wjstmddk0708@gamil.com>
2019-04-20 05:47:08 +0900

    .

--- common/js/common.js
+++ common/js/common.js
@@ -2,49 +2,45 @@ $(window).resize(function() {
 	window.location.reload();
 });
 
-
+var $eventShow = $("#eventShow");
+var $device_view = $("#device_view");
+var $mainPage = $(".mainPage");
+var $phone = $(".phone");
+var $linkBtn = $(".linkBtn");
+/*
+var $device_loading = $(".device_loading .loading");
+var $window_loading = $(".window_loading .loading");*/
+var $comLoadingWrap = $(".comLoadingWrap");
 $(document).ready(function(){
-	var $eventShow = $("#eventShow");
-	var $device_view = $("#device_view");
-	var $mainPage = $(".mainPage");
-	var $phone = $(".phone");
-	
-	var $device_loading = $(".device_loading .loading");
-	var $window_loading = $(".window_loading .loading");
-	var $comLoadingWrap = $(".comLoadingWrap");
-	
 	var flexHeight = $phone.height();
 	var flexWidth = flexHeight*0.55;
 	var $windowWidth = $(".WebView").width();
 	var flexHeight2 = $windowWidth*1.7;
 	
-	$comLoadingWrap.load("common/loading.html");
-	$window_loading.show();
-	
     if($windowWidth<flexWidth){
         $phone.width($windowWidth);
         $phone.height(flexHeight2);
         $phone.css("box-sizing","border-box");
-        $phone.removeClass("flexGrow");
-        $phone.show();
-        $window_loading.hide();
+        $phone.removeClass("flexGrow visibility_hidden");
+        $phone.addClass("visibility_show");
     }else{
         $phone.width(flexWidth);
-        $phone.show();
-        $window_loading.hide();
+        $phone.removeClass("visibility_hidden");
+        $phone.addClass("visibility_show");
+        $linkBtn.addClass("font16");
     }
     
     
-    $(".linkBtn").on("click",function(){
+    $linkBtn.on("click",function(){
         var link = "happyEvent/"+$(this).data("event")+"/index.html";
         $eventShow.attr("src",link);
         $mainPage.hide();
-        $device_loading.show();
+        $comLoadingWrap.show();
         $eventShow.show();
         
         $eventShow.on('load', function(){
         	$eventShow.removeClass("visibility_hidden");
-        	$device_loading.hide();
+        	$comLoadingWrap.hide();
         });
         
        
@@ -52,8 +48,6 @@ $(document).ready(function(){
     
     $(".btn_home").on("click",function(){
         window.location.reload();
-        $device_loading.remove();
-        $window_loading.remove();
     });
     
     
