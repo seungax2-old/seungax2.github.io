a614daa861205e1fe5de89174c69d3567d442c2b
jeonsa <wjstmddk0708@gamil.com>
2019-04-25 21:46:29 +0900

    해피포인트 완료

--- common/js/common.js
+++ common/js/common.js
@@ -4,15 +4,20 @@ $(document).ready(function(){
 		if ( filter.indexOf( navigator.platform.toLowerCase() ) < 0 ) { 
 			console.log('mobile 접속'); 
 			$(".header").height("15vw");
+			$(".content").addClass("Conmo");
 		} else { 
 			console.log('pc 접속');
 			$(".header").height("60px");
+			$(".content").addClass("Conpc");
 		}
+		var flexGallery = $(".gallery").height()*0.95;
+		$(".content .smallView .imgWrap").css("max-height",flexGallery);
 	}
 	
 	$(".hrefBtn").on("click",function(){
         var link = $(this).data("href");
         location.href=link;
     });
+	
 });
 
