2a0dbf4f0b58d50c4d84d3f71e2e749476bc8beb
jeonsa <wjstmddk0708@gamil.com>
2019-04-30 03:58:58 +0900

    높이 못잡는 오류 수정

--- common/js/common.js
+++ common/js/common.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(window).load(function(){
 	var filter = "win16|win32|win64|mac|macintel";
 	if ( navigator.platform ) { 
 		if ( filter.indexOf( navigator.platform.toLowerCase() ) < 0 ) { 
