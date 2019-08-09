21089a479b1f0a02263379d09d1eb61939e3b3c5
seungax2 <tmddk0708@naver.com>
2019-08-09 14:53:37 +0900

    threeJs getData point cloud controls logic add

--- threeJs/pointCloud/js/frameUpload.js
+++ threeJs/pointCloud/js/frameUpload.js
@@ -2,7 +2,6 @@
 // 데이터 저장할 배열 필요 일종의 q 배열
 // 데이터 실시간으로 넘어오는데로 q에 프레임 번호 넣고 담은뒤 하나씩 꺼내서 그리기
 // 매프레임마다 새로 그리기 > render 안에 요소 삭제 render.remove()
-// TODO :: 데이터저장한 배열 그린 프레임은 삭제할지 유지할지?
 
 function PointCloud()
 {
@@ -60,38 +59,44 @@ PointCloud.prototype.threeJsBaseDraw = function(){
 
 PointCloud.prototype.render = function(){
     this.renderer.render(this.scene, this.camera);
+    this.controls.update();
 }
 
 PointCloud.prototype.frameVertex = function(){
     if(Object.keys(this.vertexDataObject).length > this.frameNo ){
-        this.geometry = new THREE.Geometry(5,50,50);
-        this.geometry.dispose();
-        for(var i = 0; i <this.vertexDataObject[this.frameNo].length; i ++){
-            this.point = new THREE.Vector3();
-            //vector(float) - vx, vy, vz
-            this.point.x = this.vertexDataObject[this.frameNo][i * 3 + 0];
-            this.point.y = this.vertexDataObject[this.frameNo][i * 3 + 1];
-            this.point.z = this.vertexDataObject[this.frameNo][i * 3 + 2];
-
-            this.geometry.vertices.push(this.point);
-
-            // cololist 0~255 사이 숫자 - cr1, cg1, cb1
-            let color = new THREE.Color(0xffffff)
-            this.geometry.colors.push(color)
-        }
-        console.log(this.geometry)
-
-        this.material = new THREE.PointsMaterial({size:1, vertexColors:THREE.VertexColors});
-        this.points = new THREE.Points(this.geometry, this.material);
-        this.scene.add(this.points);
-
-        this.geometry.verticesNeedUpdate = true;
-        this.geometry.normalsNeedUpdate = true;
-
-        this.render();
+        this.vertexDataParse();
         this.scene.remove( this.points );
-        this.frameNo++;
     }else{
+        this.frameNo = this.frameNo - 1;
+        this.vertexDataParse();
         return;
     }
+}
+
+PointCloud.prototype.vertexDataParse = function(){
+    this.geometry = new THREE.Geometry(5,50,50);
+    this.geometry.dispose();
+    this.geometry.verticesNeedUpdate = true;
+    this.geometry.normalsNeedUpdate = true;
+
+    for(var i = 0; i < this.vertexDataObject[this.frameNo].length; i ++){
+        this.point = new THREE.Vector3();
+        //vector(float) - vx, vy, vz
+        this.point.x = this.vertexDataObject[this.frameNo][i * 3 + 0];
+        this.point.y = this.vertexDataObject[this.frameNo][i * 3 + 1];
+        this.point.z = this.vertexDataObject[this.frameNo][i * 3 + 2];
+
+        this.geometry.vertices.push(this.point);
+
+        // cololist 0~255 사이 숫자 - cr1, cg1, cb1
+        let color = new THREE.Color(0xffffff)
+        this.geometry.colors.push(color)
+    }
+
+    this.material = new THREE.PointsMaterial({size:1, vertexColors:THREE.VertexColors});
+    this.points = new THREE.Points(this.geometry, this.material);
+    this.scene.add(this.points);
+
+    this.render();
+    this.frameNo++;
 }
