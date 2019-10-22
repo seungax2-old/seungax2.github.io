b70fd21729781fca6b408cb9813d06907681b4aa
seungax2 <seungax2@gmail.com>
2019-10-22 14:54:09 +0900

    빠진파일 upload

--- plyFileLoader/js/plyparse.js
+++ plyFileLoader/js/plyparse.js
@@ -1,18 +1,18 @@
-function PLY(){
+function PLY() {
     this.propertyNameMapping = {}
     this.vertex = new Array();
     this.face_vertex_indices = new Array();
     this.face_texcoord = new Array();
 }
 
-PLY.prototype.parsePly = function(data){
+PLY.prototype.parsePly = function (data) {
 
     var header = this.parseHeader(data);
 
     var patternBody = /end_header\s([\s\S]*)$/
     var body = ''
     if ((result = patternBody.exec(data)) !== null) {
-        body = result[ 1 ]
+        body = result[1]
     }
 
     var lines = body.split('\n')
@@ -20,40 +20,40 @@ PLY.prototype.parsePly = function(data){
     var currentElementCount = 0
 
     for (var i = 0; i < lines.length; i++) {
-        var line = lines[ i ];
+        var line = lines[i];
         line = line.trim();
 
         if (line === '') {
             continue;
         }
 
-        if (currentElementCount >= header.elements[ currentElement ].count) {
+        if (currentElementCount >= header.elements[currentElement].count) {
             currentElement++
             currentElementCount = 0
         }
 
-        var element = this.parseASCIIElement(header.elements[ currentElement ].properties, line)
+        var element = this.parseASCIIElement(header.elements[currentElement].properties, line)
 
         currentElementCount++
     }
 
     ply = {
-        vertex : this.vertex,
-        face_vertex_indices : this.face_vertex_indices,
-        face_texcoord : this.face_texcoord
+        vertex: this.vertex,
+        face_vertex_indices: this.face_vertex_indices,
+        face_texcoord: this.face_texcoord
     }
     return ply
 }
 
-PLY.prototype.parseHeader = function(data){
+PLY.prototype.parseHeader = function (data) {
     var patternHeader = /ply([\s\S]*)end_header\s/
     var headerText = ''
     var headerLength = 0
     var result = patternHeader.exec(data)
 
     if (result !== null) {
-        headerText = result[ 1 ]
-        headerLength = result[ 0 ].length
+        headerText = result[1]
+        headerLength = result[0].length
     }
 
     var header = {
@@ -65,28 +65,28 @@ PLY.prototype.parseHeader = function(data){
     var lines = headerText.split('\n')
     var currentElement, lineType, lineValues
 
-    function makePlyElementProperty (propertValues, propertyNameMapping) {
+    function makePlyElementProperty(propertValues, propertyNameMapping) {
         var property = {
-            type: propertValues[ 0 ]
+            type: propertValues[0]
         }
 
         if (property.type === 'list') {
-            property.name = propertValues[ 3 ]
-            property.countType = propertValues[ 1 ]
-            property.itemType = propertValues[ 2 ]
+            property.name = propertValues[3]
+            property.countType = propertValues[1]
+            property.itemType = propertValues[2]
         } else {
-            property.name = propertValues[ 1 ]
+            property.name = propertValues[1]
         }
 
         if (property.name in propertyNameMapping) {
-            property.name = propertyNameMapping[ property.name ]
+            property.name = propertyNameMapping[property.name]
         }
 
         return property
     }
 
     for (var i = 0; i < lines.length; i++) {
-        var line = lines[ i ]
+        var line = lines[i]
         line = line.trim()
 
         if (line === '') {
@@ -99,8 +99,8 @@ PLY.prototype.parseHeader = function(data){
 
         switch (lineType) {
             case 'format':
-                header.format = lineValues[ 0 ]
-                header.version = lineValues[ 1 ]
+                header.format = lineValues[0]
+                header.version = lineValues[1]
                 break;
 
             case 'comment':
@@ -112,8 +112,8 @@ PLY.prototype.parseHeader = function(data){
                     header.elements.push(currentElement)
                 }
                 currentElement = {}
-                currentElement.name = lineValues[ 0 ]
-                currentElement.count = parseInt(lineValues[ 1 ])
+                currentElement.name = lineValues[0]
+                currentElement.count = parseInt(lineValues[1])
                 currentElement.properties = []
                 break;
 
@@ -133,27 +133,27 @@ PLY.prototype.parseHeader = function(data){
     return header;
 }
 
-PLY.prototype.parseASCIIElement = function(properties, line){
+PLY.prototype.parseASCIIElement = function (properties, line) {
     var values = line.split(/\s+/)
 
     for (var i = 0; i < properties.length; i++) {
-        if (properties[ i ].type === 'list') {
+        if (properties[i].type === 'list') {
             var list = []
-            var n = this.parseASCIINumber(values.shift(), properties[ i ].countType)
+            var n = this.parseASCIINumber(values.shift(), properties[i].countType)
 
-            if(properties[ i ].name == 'texcoord'){
+            if (properties[i].name == 'texcoord') {
                 for (var j = 0; j < n; j++) {
-                    this.face_texcoord.push(this.parseASCIINumber(values.shift(), properties[ i ].itemType));
+                    this.face_texcoord.push(this.parseASCIINumber(values.shift(), properties[i].itemType));
                 }
-            }else if(properties[ i ].name == 'vertex_indices'){
+            } else if (properties[i].name == 'vertex_indices') {
                 for (var j = 0; j < n; j++) {
-                    list.push(this.parseASCIINumber(values.shift(), properties[ i ].itemType))
+                    list.push(this.parseASCIINumber(values.shift(), properties[i].itemType))
                 }
                 this.face_vertex_indices.push(list)
             }
         } else {
-            if(properties[ i ].name == 'x' || properties[ i ].name == 'y' || properties[ i ].name == 'z'){
-                this.vertex.push(this.parseASCIINumber(values.shift(), properties[ i ].type))
+            if (properties[i].name == 'x' || properties[i].name == 'y' || properties[i].name == 'z') {
+                this.vertex.push(this.parseASCIINumber(values.shift(), properties[i].type))
             }
         }
     }
@@ -164,10 +164,10 @@ PLY.prototype.parseASCIINumber = function (n, type) {
         case 'char': case 'uchar': case 'short': case 'ushort': case 'int': case 'uint':
         case 'int8': case 'uint8': case 'int16': case 'uint16': case 'int32': case 'uint32':
 
-        return parseInt(n)
+            return parseInt(n)
 
         case 'float': case 'double': case 'float32': case 'float64':
 
-        return parseFloat(n)
+            return parseFloat(n)
     }
 }
